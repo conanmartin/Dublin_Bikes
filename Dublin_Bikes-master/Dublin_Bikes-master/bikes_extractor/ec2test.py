d2472469fb9d0fa64bd7efab0ee962cf0ab16709
conanmartin <conanmartin@gmail.com>
2017-04-05 10:33:03 +0100

    app.py now connecting successfully to data base and returning data from the static table (for map markers) and dynamic table, for charts. Further changes to be made to dynamic data call to return data for specified times/dates.

--- Dublin_Bikes-master/Dublin_Bikes-master/bikes_extractor/ec2test.py
+++
@@ -1,1 +0,0 @@
-print("Hello there! You've managed to get this working on the ec2 instance.")
--- flaskapp/flaskapp/app.py
+++ flaskapp/flaskapp/app.py
@@ -1,46 +1,46 @@
 '''
 Created on 31 Mar 2017
  
-@author: user 
+@authors: David, Conan, Karl
 '''
 
 from flask import Flask, jsonify, render_template
 from sqlalchemy import create_engine
 import pymysql 
-import json
+import simplejson as json
 from pprint import pprint
 app =Flask(__name__, static_url_path='')
 
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+	return render_template("index.html")
     
 def connect_to_database():
-    engine = create_engine("mysql+pymysql://DavidSurridge:MyGerryAdams@dublinbikesmysql.cqcpf75mkbbq.us-west-2.rds.amazonaws.com:3306/myDublinBikes", echo = False)
-    return engine
+	engine = create_engine("mysql+pymysql://DavidSurridge:MyGerryAdams@dublinbikesmysql.cqcpf75mkbbq.us-west-2.rds.amazonaws.com:3306/myDublinBikes", echo = False)
+	return engine
 
 @app.route("/stations")
 def get_stations():
-    engine = connect_to_database()
-    stations = []
-    rows = engine.execute("SELECT number from bikes_static")
-    for row in rows:
-        stations.append(dict(row))
-    
+	engine = connect_to_database()
+	conn = engine.connect()
+	stations = []
+	rows = conn.execute("SELECT * from bikes_static")
+	for row in rows:
+		stations.append(dict(row))
 
-    return jsonify(stations=stations)
+	return jsonify(stations=stations)
 
 @app.route("/available/<int:station_id>")
 def get_dynamic_stations(station_id):
-    engine = connect_to_database()
-    data = []
-    rows = engine.execute("SELECT available_bikes from stations where number = {};".format(station_id))
-    for row in rows:
-        data.append(dict(row))
+	engine = connect_to_database()
+	conn = engine.connect()
+	data = []
+	rows = conn.execute("SELECT available_bikes from bikes_dynamic where number = {};".format(station_id))
+	for row in rows:
+		data.append(dict(row))
 
-
-    return jsonify(available=data) 
+	return jsonify(available=data)
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
