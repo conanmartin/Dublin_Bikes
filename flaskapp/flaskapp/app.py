b658db0e232205fc855129e081c79da7255cbf03
conanmartin <conanmartin@gmail.com>
2017-04-06 11:53:58 +0100

    Minor updates - started implementing charts from data returned for weekly_chart.

--- flaskapp/flaskapp/app.py
+++ flaskapp/flaskapp/app.py
@@ -25,7 +25,7 @@ def get_stations():
 	engine = connect_to_database()
 	conn = engine.connect()
 	stations = []
-	rows = conn.execute("SELECT * from bikes_static")
+	rows = conn.execute("SELECT * from bikes_static ORDER BY address;")
 	for row in rows:
 		stations.append(dict(row))
 
@@ -36,16 +36,7 @@ def get_dynamic_stations(station_id):
 	engine = connect_to_database()
 	conn = engine.connect()
 	data = []
-	# SELECT
-	# number, dayofweek(last_update), avg(available_bikes)
-	# FROM
-	# myDublinBikes.bikes_dynamic
-	# where
-	# number = 5 and time(last_update) > '05:00:00'
-	# group
-	# by
-	# dayofweek(last_update);
-	rows = conn.execute("SELECT number, dayofweek(last_update), avg(available_bikes) from bikes_dynamic where time(last_update) > '05:00:00' and number = {} group by dayofweek(last_update);".format(station_id))
+	rows = conn.execute("SELECT dayofweek(last_update), avg(available_bikes) from bikes_dynamic where time(last_update) > '05:00:00' and number = {} group by dayofweek(last_update);".format(station_id))
 	for row in rows:
 		data.append(dict(row))
 
